9a6a1d34b27d8dd72da05aa34217f9c036a5c76c
Nicolas Rodriguez <mmorodriguez@outlook.com>
2023-09-26 00:12:03 -0300

    Mock refactorizado

--- app/services/mock_service.py
+++ app/services/mock_service.py
@@ -1,42 +1,107 @@
 from services.db_service import db_service
 from datetime import datetime
+from sqlmodel import select
 from models import User, Restaurant, Category, Dish, SideDish, Table, Waiter, SideDishOptions, Order, OrderDetail
 import random
 
 names = ["Pepe", "Juan", "Pablo", "Fede", "Marcos", "Martin", "Monica", "Lucia", "Paola", "Victoria", "Martina", "Daniela", "Agostina", "Agustin", "Gustavo", "La Meles"]
-order_states = ["processing", "waiting", "preparation", "cancelled", "delivered"]
-platos_pricipales = ["Milanesa al plato", "Huevo frito", "Sushi", "Pollo a la mostaza", "Tortilla de papa", "Pizza"]
-sanguches = ["Pebete", "Lomito", "Lomo ultra mega XXL", "Pizzalomo", "Sanguche de milanesa", "Hamburgesa", "Hamburguesa de hongos"]
-pastas = ["Fideos con tuco", "Lasagña", "Ravioles caseros", "Sorrentinos"]
-rellenos = [ "Tarta de acelga", "Tarta de choclo", "Empanadas criollas", "Empanadas arabes", "Empandas de atun", "Empanadas de pollo", "Empanadas de acelga", "Empanadas de soja", "Tacos"]
-side_dishes = ["Papas fritas", "Pure", "Ensalada", "Papas al horno", "Ensalada rusa", "Rabas"]
-drinks = ["Cocucha bien fria", "Priteado", "Prity", "Fernandito", "Birrita bien helada", "Levite", "Jugo de naranja exprimida", "Limonada", "Agua mineral","Gin tonic", "Sprite", "Fanta"]
+order_states_names = ["processing", "waiting", "preparation", "cancelled", "delivered"]
+platos_pricipales_names = ["Milanesa al plato", "Huevo frito", "Sushi", "Pollo a la mostaza", "Tortilla de papa", "Pizza"]
+sanguches_names = ["Pebete", "Lomito", "Lomo ultra mega XXL", "Pizzalomo", "Sanguche de milanesa", "Hamburgesa", "Hamburguesa de hongos"]
+pastas_names = ["Fideos con tuco", "Lasagña", "Ravioles caseros", "Sorrentinos"]
+rellenos_names = [ "Tarta de acelga", "Tarta de choclo", "Empanadas criollas", "Empanadas arabes", "Empandas de atun", "Empanadas de pollo", "Empanadas de acelga", "Empanadas de soja", "Tacos"]
+side_dishes_names = ["Papas fritas", "Pure", "Ensalada", "Papas al horno", "Ensalada rusa", "Rabas"]
+drinks_names = ["Cocucha bien fria", "Priteado", "Prity", "Fernandito", "Birrita bien helada", "Levite", "Jugo de naranja exprimida", "Limonada", "Agua mineral","Gin tonic", "Sprite", "Fanta"]
 
 def create_mocks_in_db():
-    db_service.create_object(User(id = 1, user = "admin", password = "admin", email = "admin@seminario.com"))
-    db_service.create_object(Restaurant(id = 1, name = "Pepe", last_name = "Argento", user = 1))
-    db_service.create_object(Category(id = 1, name = "Bebidas", restaurant = 1))
-    db_service.create_object(Category(id = 2, name = "Sanguches", restaurant = 1))
-    db_service.create_object(Category(id = 3, name = "Pastas", restaurant = 1))
-    db_service.create_object(Category(id = 4, name = "Rellenos", restaurant = 1))
-    db_service.create_object(Category(id = 5, name = "Platos principales", restaurant = 1))
-    for i in range(6):
-        db_service.create_object(Dish(id = 1+5*i,name = random.choice(platos_pricipales),description = "Wea re rica",preparation_time = random.randint(15,45),category = 5,price = random.randint(2000,7000)))
-        db_service.create_object(Dish(id = 2+5*i,name = random.choice(sanguches),description = "Wea re grasosa",preparation_time = random.randint(5,30),category = 2,price = random.randint(1000,5000)))
-        db_service.create_object(Dish(id = 3+5*i,name = random.choice(pastas),description = "Como los que hacia la nona :(",preparation_time = random.randint(20,45),category = 3,price = random.randint(3000,8000)))
-        db_service.create_object(Dish(id = 4+5*i,name = random.choice(rellenos),description = "Wea re ancha",reparation_time = random.randint(5,30),category = 4,price = random.randint(1500,3000)))
-        db_service.create_object(Dish(id = 5+5*i,name = random.choice(drinks),description = "Wea re refrescante",preparation_time = random.randint(0,5),category = 1,price = random.randint(700,4000)))
-        db_service.create_object(SideDish(id = 1+2*i,name = random.choice(side_dishes),description = "Wea para acompañar",extra_price = random.randint(500,1500)))
-        db_service.create_object(SideDish(id = 2+2*i,name = random.choice(side_dishes),description = "Wea para acompañar",extra_price = random.randint(500,1500)))
-        db_service.create_object(SideDishOptions(dish = 1+5*i,side_dish = 1+2*i))
-        db_service.create_object(SideDishOptions(dish = 2+5*i,side_dish = 2+2*i))
-    for i in range(15):
-        db_service.create_object(Table(restaurant = 1))
-    for i in range(3):
-        db_service.create_object(Waiter(name = random.choice(names)))
-    for i in range(10):
-        price = random.randint(2000,7000)
-        db_service.create_object(Order(id = i, table = random.randint(1,15) , total = price, created_at = datetime.now(), state = random.choice(order_states)))
-        db_service.create_object(OrderDetail(dish_selected = random.randint(1,35), order = i, subtotal = price, customer = random.choice(names)))
 
+    #Crear usuario
+    user_1 = User(user = "admin", password = "admin", email = "admin@seminario.com")
+    db_service.create_object(user_1)
+
+    #Crear restaurante
+    statement = select(User).where(User.user == "admin")
+    user: User = db_service.get_with_filters(statement)[0]
+    restaurant_1 = Restaurant(name = "Pepe", last_name = "Argento", user = user.id)
+    db_service.create_object(restaurant_1)
+
+    #Crear categorias
+    statement = select(Restaurant).where(Restaurant.name == "Pepe")
+    restaurant: Restaurant = db_service.get_with_filters(statement)[0]
+    category_1 = Category(name = "Bebidas", restaurant = restaurant.id)
+    category_2 = Category(name = "Sanguches", restaurant = restaurant.id)
+    category_3 = Category(name = "Pastas", restaurant = restaurant.id)
+    category_4 = Category(name = "Rellenos", restaurant = restaurant.id)
+    category_5 = Category(name = "Platos principales", restaurant = restaurant.id)
+    db_service.create_object(category_1)
+    db_service.create_object(category_2)
+    db_service.create_object(category_3)
+    db_service.create_object(category_4)
+    db_service.create_object(category_5)
+
+    #Crear platos
+    statement = select(Category).where(Category.name == "Platos principales")
+    plato_principal: Category = db_service.get_with_filters(statement)[0]
+
+    statement = select(Category).where(Category.name == "Sanguches")
+    sanguche: Category = db_service.get_with_filters(statement)[0]
+
+    statement = select(Category).where(Category.name == "Pastas")
+    pasta: Category = db_service.get_with_filters(statement)[0]
+
+    statement = select(Category).where(Category.name == "Rellenos")
+    relleno: Category = db_service.get_with_filters(statement)[0]
+
+    statement = select(Category).where(Category.name == "Bebidas")
+    bebida: Category = db_service.get_with_filters(statement)[0]
+
+    for i in range(6):
+        dish_1 = Dish(name = random.choice(platos_pricipales_names),description = "Wea re rica",preparation_time = random.randint(15,45),category = plato_principal.id, price = random.randint(2000,7000))
+        dish_2 = Dish(name = random.choice(sanguches_names),description = "Wea re grasosa",preparation_time = random.randint(5,30),category = sanguche.id, price = random.randint(1000,5000))
+        dish_3 = Dish(name = random.choice(pastas_names),description = "Como los que hacia la nona :(",preparation_time = random.randint(20,45),category = pasta.id, price = random.randint(3000,8000))
+        dish_4 = Dish(name = random.choice(rellenos_names),description = "Wea re ancha",reparation_time = random.randint(5,30),category = relleno.id, price = random.randint(1500,3000))
+        dish_5 = Dish(name = random.choice(drinks_names),description = "Wea re refrescante",preparation_time = random.randint(0,5),category = bebida.id ,price = random.randint(700,4000))
+        db_service.create_object(dish_1)
+        db_service.create_object(dish_2)
+        db_service.create_object(dish_3)
+        db_service.create_object(dish_4)
+        db_service.create_object(dish_5)
+
+    #Crear guarniciones
+        side_dish_1 = SideDish(name = random.choice(side_dishes_names),description = "Wea para acompañar",extra_price = random.randint(500,1500))
+        side_dish_2 = SideDish(name = random.choice(side_dishes_names),description = "Wea para acompañar",extra_price = random.randint(500,1500))
+        db_service.create_object(side_dish_1)
+        db_service.create_object(side_dish_2)
+
+    #Adjuntar guarniciones
+        side_dishes: SideDish = db_service.get_list_from_db(SideDish)
+        dishes: Dish = db_service.get_list_from_db(Dish)
+        side_dish_option_1 = SideDishOptions(dish = random.choice(dishes).id, side_dish = random.choice(side_dishes).id)
+        side_dish_option_2 = SideDishOptions(dish = random.choice(dishes).id, side_dish = random.choice(side_dishes).id)
+        db_service.create_object(side_dish_option_1)
+        db_service.create_object(side_dish_option_2)
+
+    #Crear tablas
+    for i in range(15):
+        table_1 = Table(restaurant = restaurant.id)
+        db_service.create_object(table_1)
+
+    #Crear mosos
+    for i in range(3):
+        waiter_1 = Waiter(name = random.choice(names))
+        db_service.create_object(waiter_1)
+
+    #Cear ordenes
+    for i in range(10):
+        tables: Table = db_service.get_list_from_db(Table)
+        dishes: Dish = db_service.get_list_from_db(Dish)
+        dish = random.choice(dishes)
+
+        db_service.create_object(Order(table = random.choice(tables).id , total = dish.price, created_at = datetime.now(), state = random.choice(order_states_names)))
+
+    #Crear detalles de ordenes
     
+        statement = "SELECT MAX(id) FROM pedidos "
+        bebida: Category = db_service.get_with_filters(statement)[0]
+    
+        db_service.create_object(OrderDetail(dish_selected = dish.id, order = i, subtotal = dish.price, customer = random.choice(names)))
