1abc2e486e550d957b72a9a2ba208d24e0cbf4ca
JCaeta <63880187+JCaeta@users.noreply.github.com>
2024-01-29 16:08:30 -0300

    Increase time in reload.bat

--- app/controlers/mock.py
+++ app/controlers/mock.py
@@ -7,5 +7,9 @@ mock_router = APIRouter(prefix="/mock", tags=["Mock"])
 @mock_router.post("/")
 async def create_mock():
     return create_mocks_2()
+<<<<<<< Updated upstream
     #return {"message": "deshabilitado temporalmente xD"}
+=======
+    # return {"message": "deshabilitado temporalmente xD"}
+>>>>>>> Stashed changes
 
--- reload.ps1
+++ reload.ps1
@@ -1,7 +1,7 @@
 Start-Process -FilePath "C:\WINDOWS\system32\cmd.exe" -ArgumentList "/c docker compose down  && docker compose up"
 Start-Sleep 16
 $containerProcess = Start-Process -FilePath "C:\WINDOWS\system32\cmd.exe" -ArgumentList '/c docker exec -it QResto_back bash -c "python -m alembic upgrade head"' -PassThru
-Start-Sleep 6
+Start-Sleep 12
 $url = "http://localhost:8000/mock"
 Invoke-RestMethod -Uri $url -Method Post
 
